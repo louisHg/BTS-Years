79efa3734fefa634cf59fcf3324275792698dc4d
LouisHg <88277444+louisHg@users.noreply.github.com>
2021-08-27 21:44:50 +0200

    Update config.js

--- Bot-discord/config.js
+++ Bot-discord/config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NTY1NDUzMDA3MjIzNzgzNDU0.XK2pQA._qBhIdbjjHlVFoJbaehuiB3dZnM';
+exports.TOKEN = 'XXXXX';
 
 exports.PREFIX = '!';
 
-exports.GOOGLE_API_KEY = 'AIzaSyAy2idA1tghhftsTWWv7OaYjOv_FqTUPSE';
+exports.GOOGLE_API_KEY = 'XXXX';
